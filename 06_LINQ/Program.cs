ef952f7a496952c23447e6bf7ef6b3e5d58debfc
joaor <joaor@DESKTOP-ORTRKJA>
2018-07-29 03:11:10 -0300

    Criando tipo de LINQ com ARRAY.

--- 06_LINQ/Program.cs
+++ 06_LINQ/Program.cs
@@ -10,6 +10,16 @@ namespace _06_LINQ
     {
         static void Main(string[] args)
         {
+            int[] lista = {3, 4, 8, 20, 15, 80, 90, 70, 60, 50};
+
+            var listaFiltrada = lista.Where(a => a > 10).OrderBy(a => a).Select(a => a);
+
+            foreach (var item in listaFiltrada)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
